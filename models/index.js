c22e28acf06c58568d10f6b67c39e55d35882da8
Shaun Nelson <shaunptnelson@gmail.com>
2023-06-13 02:50:53 -0400

    Added table relationships

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const Blog = require('./Blog');
+const User = require('./User');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { Blog, User };
