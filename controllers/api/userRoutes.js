e3dc94890e6780db5a0d84a2c97544253ce8a2d9
Shaun Nelson <shaunptnelson@gmail.com>
2023-06-13 17:12:22 -0400

    Added logout functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
       res.status(200).json(userData);
     });
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
       res.json({ user: userData, message: 'You are now logged in!' });
     });
@@ -52,7 +52,8 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  console.log();
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
